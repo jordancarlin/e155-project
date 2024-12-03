f159325be8336c7c45ac0c174086a166c4bb9b4a
Jordan Carlin <jordanmcarlin@gmail.com>
2024-12-03 02:15:08 -0800

    Attempt to only store 200x200

--- fpga/src/pixelStore.sv
+++ fpga/src/pixelStore.sv
@@ -5,12 +5,21 @@ module pixelStore (input  logic clk,
                    input  logic [2:0] newColor,
                    output logic [2:0] colorCode);
 
+  logic [2:0] colorCodeRam;
   logic [2:0] colorArray [40000:0];
 
   initial $readmemb("testcolor.mem", colorArray);
 
+  always_comb begin
+    if ($unsigned(rx) >= 200 || $unsigned(ry) >= 200) begin
+      colorCode = 3'b000;
+    end else begin
+      colorCode = colorCodeRam;
+    end
+  end
+
   always_ff @(posedge clk) begin
     if(brush) colorArray[{wy,wx}] <= newColor;
-    colorCode <= colorArray[{ry[7:0],rx[7:0]}];
+    colorCodeRam <= colorArray[{ry[7:0],rx[7:0]}];
   end
 endmodule
