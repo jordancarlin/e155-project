b35a223e17671009940cc35f4b149e10bb53b467
Jordan Carlin <jordanmcarlin@gmail.com>
2024-12-03 19:20:37 -0800

    Attempt to center screen

--- fpga/src/pixelStore.sv
+++ fpga/src/pixelStore.sv
@@ -5,6 +5,14 @@ module pixelStore (input  logic clk, reset,
                    input  logic [9:0] rx, ry,
                    output logic [2:0] colorCode);
 
+  logic [7:0] rxRam, ryRam;
+
+  // 640 * 480
+  // 180 * 180
+  // 360 * 360
+
+  localparam MAX_COORDINATE = 180;
+
   logic [2:0] colorCodeRam; //
   // logic [2:0] colorArray [40000:0];
 
@@ -15,25 +23,34 @@ module pixelStore (input  logic clk, reset,
   // end
 
   always_comb begin
-    if ($unsigned(rx) >= 200 || $unsigned(ry) >= 200) begin
+    rxRam = rx - 230;
+    ryRam = ry - 150;
+
+    if (rxRam < 0 | ryRam < 0)
       colorCode = 3'b101;
-    end else begin
+    else if (rxRam >= MAX_COORDINATE | ryRam >= MAX_COORDINATE)
+      colorCode = 3'b101;
+    else
       colorCode = colorCodeRam;
-    end
   end
-  
+
+    // if ($unsigned(rx) >= MAX_COORDINATE || $unsigned(ry) >= MAX_COORDINATE) begin
+    //   colorCode = 3'b101;
+    // end else begin
+    //   colorCode = colorCodeRam;
+    // end
+  end
+
   pixelRam pixelRam(
     .wr_clk_i(clk),
     .rd_clk_i(clk),
     .rst_i(reset),
     .wr_clk_en_i(1'b1),
     .rd_en_i(1'b1),
-    .rd_clk_en_i(1'b1), 
-    .wr_en_i(brush), 
-    .wr_data_i(newColor), 
+    .rd_clk_en_i(1'b1),
+    .wr_en_i(brush),
+    .wr_data_i(newColor),
     .wr_addr_i({wy[6:0],wx}),
-    .rd_addr_i({ry[6:0],rx[7:0]}),
-    .rd_data_o(colorCodeRam)); 
-  
-  
+    .rd_addr_i({ryRam[6:0],rxRam[7:0]}),
+    .rd_data_o(colorCodeRam));
 endmodule
