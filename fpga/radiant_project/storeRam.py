79ea5eadee64f57980aa6ed5201213660b89c4f5
Jordan Carlin <jordanmcarlin@gmail.com>
2024-12-04 17:03:12 -0800

    Fix ram

--- fpga/radiant_project/storeRam.py
+++ fpga/radiant_project/storeRam.py
@@ -2,7 +2,7 @@ toWriteTo = open("testcolor.mem", "w")
 
 for i in range(128):
   for j in range(128):
-    if (i/16)%2 == 0:
+    if (j // 16)%2 == 0:
       toWriteTo.write('001\n') ## 101 is red
     else:
       toWriteTo.write('010\n') ## 010 is blue
